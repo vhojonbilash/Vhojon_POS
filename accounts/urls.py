b8d8eb232dc5bd729558cc3d2e9924b247c605d5
shahreerirfan <mdshahreerirfan@gmail.com>
2026-01-21 20:19:10 +0600

    Initial commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.admin_login, name="login"),
+    path("", views.admin_login, name="login"),
     path("logout/", views.admin_logout, name="logout"),
-    path("", views.home, name="home"),
+    path("/home", views.home, name="home"),
     path("clock/", views.server_clock, name="server_clock"),
 ]
